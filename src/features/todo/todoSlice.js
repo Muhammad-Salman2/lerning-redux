8b49fba8e22e65f158e6834e048314ee9946d314
salman <salman7official7@gmail.com>
2025-10-18 11:57:39 +0500

    updates

--- src/features/todo/todoSlice.js
+++ src/features/todo/todoSlice.js
@@ -10,9 +10,12 @@ export const todoSlice = createsSlice({
     reducers: {
         addTodo: (state, action) => {
             const todo = {id : nanoid(), text : action.payload};
+            state.todos.push(todo);
         },
         removeTodo: (state, action) => {
             state.todos = state.todos.filter(todo => todo.id !== action.payload);
         }
+
+
     },
 })
